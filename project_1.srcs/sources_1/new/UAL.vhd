6fc7a3c84ea618e9edfcab228b3d11d7f691d2d1
aldeobacchus <75472779+aldeobacchus@users.noreply.github.com>
2022-05-31 21:20:18 +0200

    VHDL commit

---
+++ project_1.srcs/sources_1/new/UAL.vhd
@@ -0,0 +1,79 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 05.04.2022 17:59:36
+-- Design Name: 
+-- Module Name: UAL - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity UAL is
+    Port ( Ctrl_Alu : in STD_LOGIC_VECTOR (2 downto 0);
+           A : in STD_LOGIC_VECTOR (7 downto 0);
+           B : in STD_LOGIC_VECTOR (7 downto 0);
+           S : out STD_LOGIC_VECTOR (7 downto 0);
+           N : out STD_LOGIC;
+           O : out STD_LOGIC;
+           Z : out STD_LOGIC;
+           C : out STD_LOGIC);
+end UAL;
+
+    -- OOO : add
+    -- 001 : sub
+    -- 010 : mul
+    -- 011 : div
+
+architecture Behavioral of UAL is
+        
+    signal Add_Res : std_logic_vector (8 downto 0) := (others => '0');
+    signal Sub_Res : std_logic_vector (7 downto 0) := (others => '0');
+    signal Mul_Res : std_logic_vector (15 downto 0):= (others => '0');
+    signal Div_Res : std_logic_vector (7 downto 0) := (others => '0');
+    
+    signal Aux : std_logic_vector (7 downto 0) := (others => '0');
+    
+begin
+    
+    Add_Res <= ('0'& A) + ('0' & B);
+    Sub_Res <= A - B;
+    Mul_Res <= A * B;
+    Div_Res <= '0'&(A(7 downto 1));
+    
+    Aux <=  Add_Res(7 downto 0) when Ctrl_Alu="000" else
+            Sub_Res when Ctrl_Alu="010" else
+            Mul_Res(7 downto 0) when Ctrl_Alu="001" else
+            Div_Res when Ctrl_Alu="011" ;
+    
+    -- FLAGS
+    C <= Add_Res(8);                                        --Carry
+    Z <= '1' when Aux = 0 else '0';                         --Zero
+    O <= '1' when Mul_Res(15 downto 0) /= X"00" and Ctrl_Alu="001" else '0';   --Overflow
+    N <= '1' when A < B and Ctrl_Alu="010" else '0';        --Negative
+    
+    S <= Aux;
+        
+end Behavioral;
